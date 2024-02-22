6d10f3ebccd5f354d331910b3058701968350308
ekrdg <e.karadag@outlook.de>
2024-02-22 11:15:54 +0100

    Update microphone.py

--- microphone.py
+++ microphone.py
@@ -6,12 +6,12 @@ def get_audio():
     with sr.Microphone() as source:
         print("Listening...")
         audio = recognizer.listen(source)
-    return (recognize_speech(audio))
+    return (audio)
 
 def recognize_speech(audio):
     try:
         text = recognizer.recognize_google(audio)
-        print(text)
+        return text
     except sr.UnknownValueError:
         print("Sorry, I couldn't understand that.")
     except sr.RequestError:
