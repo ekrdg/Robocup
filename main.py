038d180a7bc1b26e454ad444075c4f16bea0dd1e
ekrdg <e.karadag@outlook.de>
2024-02-22 11:15:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,50 +2,54 @@ import sys
 
 from naoqi import ALProxy
 from hello import hello
-from microphone import get_audio
+from speechtest import *
 from weatherapi import weather as get_weather
-from move import moveto as move_to
+from move import *
+from stretch import *
 
-def main(robot_IP, robot_PORT=9559):
-    hello(robot_IP, robot_PORT)
-    tts = ALProxy("ALTextToSpeech", robot_IP, robot_PORT) 
-
+def main():
+    IP = "nao6.local"
+    PORT = 9559
+    tts = ALProxy("ALTextToSpeech", IP, PORT) 
+    #text = "world"
+    #tts.say("Hello "+text)
+    hello(IP,PORT)
     tts.say("Hello, ask me about the weather in any city. Are you ready?")
-    get_confirmation("You said " + get_audio() + ". Is that correct?", tts)
-
     tts.say("Great! What city would you like to know the weather for?")
-    city = get_audio()
-    get_confirmation("You said " + city + ". Is that correct?", tts)
+    a = get_audio()
+    city = recognize_speech(a)
+    #get_confirmation("Please say yes to confirm your answer")
 
-    tts.say("Great! I will now check the weather for " + city + ".")
+    tts.say("Great! I will now check the weather for "+ str(city))
     temperature = get_weather(city)
-    tts.say("The weather in " + city + " is "+ temperature + " degrees celsius.")
+    tts.say("The weather in " + str(city) + " is "+ str(temperature) + " degrees celsius")
+    
+    tts.say("Would you like me to visualize the weather in " + str(city))
+    input = get_audio()
+    answer = recognize_speech(input)
 
-    tts.say("Would you like me to visualize the weather in " + city + "?")
-    visualization = get_audio()
-
-    if visualization == "yes":
-        tts.say("Great! I will now visualize the weather for " + city + ".")
+    if answer == "yes":
+        tts.say("Great! I will now visualize the weather for " + str(city))
+        #point_to_city(city)
         move_to(temperature)
     else:
-        tts.say("Okay, I will not visualize the weather for " + city + ".")
+        tts.say("Okay, I will not visualize the weather for " + str(city) + ".")
+        tts.say("Thank you for using the weather service. Have a great day!")
 
-    tts.say("Thank you for using the weather service. Have a great day!")
 
-
-def get_confirmation(prompt, tts):
-    tts.say(prompt)
-    answer = get_audio()
-    
-    while answer != "yes":
-        tts.say("I'm sorry, I didn't understand that. Please try again, for confirmation say yes, to end the program say stop.")
-        if answer == "stop":
-            tts.say("Goodbye!")
-            sys.exit()
-        answer = get_audio()
-    return answer
+def get_confirmation(prompt):
+    print(prompt)
+    input = get_audio()
+    answer = recognize_speech(input)
+    if answer =="yes":
+        print("you said yes")
+    if answer == "no":
+        #print("I'm sorry, I didn't understand that. Please try again, for confirmation say yes, to end the program say stop.")
+        print("You said no")
+    if answer == "stop":
+        print("Goodbye!")
+        sys.exit()
 
       
 if __name__ == "__main__":
-    robot_IP = ""
-    main(robot_IP)
+    main()
